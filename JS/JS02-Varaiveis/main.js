242fa7700c63e9c6d7f626c4dbc248b62b169d3d
Ayrtonley1402 <ayrtonreisdonascimento@gmail.com>
2024-05-08 21:44:39 -0300

    JS02-Variaveis

---
+++ JS/JS02-Varaiveis/main.js
@@ -0,0 +1,41 @@
+// Objeto console//
+console.log("Hello World")
+// Quando trabalho com texto coloco dentro de aspas duplas ou simples
+
+// Variaveis
+// quero criar um espaço na memoria do computador chamado _felippe e dizer que esse espaço na memória irá receber o valor 8
+var _felippe = 8
+var _felippe = "Ronaldinho" // redeclarar uma variavel e novo valor 
+console.log(_felippe) 
+
+let letsGo = 4  // com o let não se pode redeclarar, mas pode ser adicionado novos valores
+letsGo = 3
+console.log(letsGo)
+
+console.clear()
+
+// const não pode ser redeclarada nem dar um novo valor pra ela
+const PI = 3.1415
+console.log(PI)
+
+console.clear()
+
+let NAN = 90/2
+console.log(NAN)
+
+console.clear()
+
+let Corinthians = "Time do povo" //variavael recebeu uma string "texto"
+console.log("Time" + "do" + "povo") //concatenar
+
+var numero1 = 2
+var texto = "Eu tenho"
+var texto2 = "Reais"
+console.log(texto + numero1 + texto2)
+console.clear()
+
+// valores booleanos
+var num1 = 5
+var num2 = 5
+console.log(num1 == num2)
+
