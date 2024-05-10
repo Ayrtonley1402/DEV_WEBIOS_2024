caf5fb400487ca526eb5f132d92835be18d79a3c
Ayrtonley1402 <ayrtonreisdonascimento@gmail.com>
2024-05-09 21:42:51 -0300

    ExercicioCasa JS

---
+++ JS/JS02-Varaiveis/ExercicioCasa/main.js
@@ -0,0 +1,6 @@
+var nota1 = 5
+var nota2 = 7
+var nota3 = 9
+var nota4 = 8
+var resultado = (nota1 + nota2 + nota3 + nota4) / 4
+console.log("A média do aluno coma as 4 notas é: " + resultado)
--- JS/JS02-Varaiveis/main.js
+++ JS/JS02-Varaiveis/main.js
@@ -38,4 +38,51 @@ console.clear()
 var num1 = 5
 var num2 = 5
 console.log(num1 == num2)
+console.clear()
+
+//Operadores
+
+var operador1 = 8
+var operador2 = null
+
+console.log(operador1 * operador2) //8 * null
+
+var operador3 = "5"
+var operador4 = 1
+console.log(operador3 - operador4)//"5" - 1
+
+var operador5 = "5"
+var operador6 = 1
+console.log(operador5 + operador6)//"5" + 1
+
+var operador7 = "five"
+var operador8 = 2
+console.log(operador8 * operador8)//"five" * 2
+console.clear()
+
+var valor1 = 30
+var valor2 =  3
+var resultado = valor1 % valor2
+console.log(resultado)
+console.clear()
+
+var compara = 6 > 7 ==4// false pq 6 já n é maior q 7, nem 7 igual a 4
+var teste = !compara//true pq o ! muda o resultado
+console.log(compara)
+console.clear()
+
+let valor3 = 4
+let valor4 = 6
+let resultado1 = valor3 > valor4 && valor3 == valor4
+//|| true ou false = true
+//& true e false = false
+//&  true e true = false
+//& false e false = false
+console.log(resultado1)
+
+var valor5 = "4"
+console.log(typeof valor5)
+
+var marley = "EU TE AMO"
+console.log(marley)
 
