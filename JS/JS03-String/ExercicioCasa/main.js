5e48a1efe44f3548ce5b76273c429523a3cbd973
Ayrtonley1402 <ayrtonreisdonascimento@gmail.com>
2024-05-18 15:36:48 -0300

    ExercícioCasa-JS03

---
+++ JS/JS03-String/ExercicioCasa/main.js
@@ -0,0 +1,5 @@
+var alunos = "Estamos aprendendo JavaScrpit e seremos futuros Devs"
+console.log(alunos.charAt(19))
+console.log(`A frase tem ${alunos.length} caracteres`)
+console.log(`${alunos.toUpperCase()}`)
+console.log(`${alunos.substring(48)}`)
