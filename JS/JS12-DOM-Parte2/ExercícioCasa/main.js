2574937636a8bd05fc1909a5ad67c8f8375d4655
Ayrtonley1402 <ayrtonreisdonascimento@gmail.com>
2024-06-07 21:39:17 -0300

    JS12-ExercícioCasa

---
+++ JS/JS12-DOM-Parte2/ExercícioCasa/main.js
@@ -0,0 +1,8 @@
+
+function criaImagem(){
+    const span = document.getElementById("img1");
+    const colocaImg = document.createElement("img");
+    img.src = onclick("Digite o nome ou link da imagem: ");
+    
+    div.appendChild(image);
+}
